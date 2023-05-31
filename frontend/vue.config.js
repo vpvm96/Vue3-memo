9d5aacc4f566873de3b9b2265f15b74474a217cf
vpvm96 <vpvm96@gmail.com>
2023-05-31 17:25:12 +0900

    feat: CORS 에러 해결

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,4 +2,11 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
 });
